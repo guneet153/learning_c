7f5ed2ae31c1771a194f066e359c46264151733d
guneet153 <guneetk1811@gmail.com>
2025-09-10 19:55:46 +0530

    commit

---
+++ Day 16/palindrome.c
@@ -0,0 +1,21 @@
+//Write a program to check if a number is a palindrome.
+
+#include<stdio.h>
+int main ()
+{
+    int n, r, sum=0, temp;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum=(sum*10)+r;
+        n=n/10;
+    }
+    if(temp==sum)
+        printf("The number is a palindrome.");
+    else
+        printf("The number is not a palindrome.");
+    return 0;
+}
