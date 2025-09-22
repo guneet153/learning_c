cd0dbc46c0bf7b7b0a1a0f1282887d6929c00421
guneet153 <guneetk1811@gmail.com>
2025-09-22 12:05:22 +0530

    Update prime.c

--- Day 17/prime.c
+++ Day 17/prime.c
@@ -2,27 +2,22 @@
 #include<stdio.h>
 int main()
 {
-    int num, i, flag = 0;
+    int i,n,a:
     printf("Enter a positive integer: ");
-    scanf("%d", &num);
-
-    // 0 and 1 are not prime numbers
-    if (num <= 1) {
-        flag = 1;
-    } else {
-        for (i = 2; i <= num / 2; ++i) {
-            // condition for non-prime number
-            if (num % i == 0) {
-                flag = 1;
+    scanf("%d", &n);
+    a=2;
+    while(a<=n-1)
+        {
+            if(n%a==0)
+            {
+                printf("%d is not aprime number \n",n);
                 break;
             }
+            a++;
         }
-    }
-
-    if (flag == 0)
-        printf("%d is a prime number.\n", num);
-    else
-        printf("%d is not a prime number.\n", num);
-
-    return 0;
+    if(a==n)
+    {
+        printf("%d is a prime number \n",n)
+            }
+ return 0;
 }
