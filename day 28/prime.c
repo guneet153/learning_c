a9c72c15075f15358ccfba7faccfb937667c87a2
guneet153 <guneetk1811@gmail.com>
2025-09-23 10:50:03 +0530

    commit

---
+++ day 28/prime.c
@@ -0,0 +1,24 @@
+//Write a program to print all the prime numbers from 1 to n
+#include<Stdio.h>
+int main()
+{
+    int n, i, j, flag;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    printf("Prime numbers between 1 and %d are: \n", n);
+    for(i=2; i<=n; i++)
+    {
+        flag = 0;
+        for(j=2; j<=i/2; j++)
+        {
+            if(i%j == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0)
+            printf("%d ", i);
+    }
+    return 0;
+}
