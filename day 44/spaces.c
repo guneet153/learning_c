3aeb1d282bae3a0c434174f5492924a5387bf021
guneet153 <guneetk1811@gmail.com>
2025-10-09 12:35:40 +0530

    commit

---
+++ day 44/spaces.c
@@ -0,0 +1,31 @@
+//count spaces ,digits and special characters in string
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i, len, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    len = strlen(str);
+    if(str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+
+    for(i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if(isPalindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
